3deb19fc042a16ee374cf79cf77d47fd9c02e51b
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-08-17 00:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
   // your code here
+	for (let i=0; i<text.length; i++) {
+		if (text.charAt(i) != ' ') {
+			return text.charAt(i);
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+const text = prompt("Enter text:");
+alert(firstChar(text)); 
